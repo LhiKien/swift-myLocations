13d35dd6f6fc7ada6ec44a9c1e0e35e4f8e25352
kickinbahk <kickinbahk@gmail.com>
2016-10-21 08:42:46 -0700

    update map to automatically zoom on users locations

--- MyLocations/MapViewController.swift
+++ MyLocations/MapViewController.swift
@@ -11,6 +11,10 @@ class MapViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     updateLocations()
+    
+    if !locations.isEmpty {
+      showLocations()
+    }
   }
   
   @IBAction func showUser() {
@@ -20,7 +24,8 @@ class MapViewController: UIViewController {
   }
   
   @IBAction func showLocations() {
-    
+    let theRegion = region(for: locations)
+    mapView.setRegion(theRegion, animated: true)
   }
   
   func updateLocations() {
@@ -34,7 +39,62 @@ class MapViewController: UIViewController {
     locations = try! managedObjectContext.fetch(fetchRequest)
     mapView.addAnnotations(locations)
   }
+
+  func region(for annotations: [MKAnnotation]) -> MKCoordinateRegion {
+    let region: MKCoordinateRegion
+
+    switch annotations.count {
+    case 0:
+      region = MKCoordinateRegionMakeWithDistance(
+                              mapView.userLocation.coordinate, 1000, 1000)
+    case 1:
+      let annotation = annotations[annotations.count - 1]
+      region = MKCoordinateRegionMakeWithDistance(
+                              annotation.coordinate, 1000, 1000)
+      
+    default:
+      var topLeftCoord = CLLocationCoordinate2D(latitude: -90,
+                                                longitude: 180)
+      var bottomRightCoord = CLLocationCoordinate2D(latitude: 90,
+                                                    longitude: -180)
+      
+      for annotation in annotations {
+        topLeftCoord.latitude = max(topLeftCoord.latitude,
+                                    annotation.coordinate.latitude)
+        topLeftCoord.longitude = min(topLeftCoord.longitude,
+                                     annotation.coordinate.longitude)
+        bottomRightCoord.latitude = min(bottomRightCoord.latitude,
+                                        annotation.coordinate.latitude)
+        bottomRightCoord.longitude = max(bottomRightCoord.longitude,
+                                         annotation.coordinate.longitude)
+      }
+      
+      let center = CLLocationCoordinate2D(
+        latitude: topLeftCoord.latitude - (
+                    topLeftCoord.latitude - bottomRightCoord.latitude) / 2,
+        longitude: topLeftCoord.longitude - (
+                    topLeftCoord.longitude - bottomRightCoord.longitude) / 2)
+      
+      let extraSpace = 1.1
+      let span = MKCoordinateSpan(
+        latitudeDelta: abs(topLeftCoord.latitude -
+                              bottomRightCoord.latitude) * extraSpace,
+        longitudeDelta: abs(topLeftCoord.longitude -
+          bottomRightCoord.longitude) * extraSpace)
+      
+      region = MKCoordinateRegion(center: center, span: span)
+    }
+
+    return mapView.regionThatFits(region)
+  }
+
+
+
+
+
 }
+
+
 
 extension MapViewController: MKMapViewDelegate {
   
