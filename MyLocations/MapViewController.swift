2ddf59dd5863b39d6c3719ea2e775dacfad3bfb2
kickinbahk <kickinbahk@gmail.com>
2016-10-21 07:50:26 -0700

    add mapviewcontroller

---
+++ MyLocations/MapViewController.swift
@@ -0,0 +1,23 @@
+import UIKit
+import MapKit
+import CoreData
+
+class MapViewController: UIViewController {
+  @IBOutlet weak var mapView: MKMapView!
+  
+  var managedObjectContext: NSManagedObjectContext!
+  
+  @IBAction func showUser() {
+    let region = MKCoordinateRegionMakeWithDistance(
+                              mapView.userLocation.coordinate, 1000, 1000)
+    mapView.setRegion(mapView.regionThatFits(region), animated: true)
+  }
+  
+  @IBAction func showLocations() {
+    
+  }
+}
+
+extension MapViewController: MKMapViewDelegate {
+  
+}
