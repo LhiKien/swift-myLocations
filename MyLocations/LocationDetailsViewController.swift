973eb0b27797d5f158ff6c1d253942ba8231bae6
kickinbahk <kickinbahk@gmail.com>
2016-10-21 18:31:12 -0700

    refactor large if statement in tableView(heightForRowAt) to switch statement

--- MyLocations/LocationDetailsViewController.swift
+++ MyLocations/LocationDetailsViewController.swift
@@ -76,22 +76,23 @@ class LocationDetailsViewController: UITableViewController {
   // MARK: - UITableViewDelegate
   override func tableView(_ tableView: UITableView,
                           heightForRowAt indexPath: IndexPath) -> CGFloat {
-    if indexPath.section == 0 && indexPath.row == 0 {
+    switch (indexPath.section, indexPath.row) {
+    case (0, 0):
       return 88
-    } else if indexPath.section == 1 {
+    case (1, _):
       if imageView.isHidden {
         return 44
       } else {
         return 280
       }
-    } else if indexPath.section == 2 && indexPath.row == 2 {
+    case (2, 2):
       addressLabel.frame.size = CGSize(width: view.bounds.size.width - 115,
                                        height: 10000)
       addressLabel.sizeToFit()
       addressLabel.frame.origin.x = view.bounds.size.width -
-                                    addressLabel.frame.size.width - 15
+                                        addressLabel.frame.size.width - 15
       return addressLabel.frame.size.height + 20
-    } else {
+    default:
       return 44
     }
   }
