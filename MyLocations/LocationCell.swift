93319c79c966425a724a6c0a836ed2023958fdca
kickinbahk <kickinbahk@gmail.com>
2016-10-24 15:21:53 -0700

    fix other colors in the app.

--- MyLocations/LocationCell.swift
+++ MyLocations/LocationCell.swift
@@ -17,6 +17,9 @@ class LocationCell: UITableViewCell {
     descriptionLabel.highlightedTextColor = descriptionLabel.textColor
     addressLabel.textColor = UIColor(white: 1.0, alpha: 0.4)
     addressLabel.highlightedTextColor = addressLabel.textColor
+    photoImageView.layer.cornerRadius = photoImageView.bounds.size.width / 2
+    photoImageView.clipsToBounds = true
+    separatorInset = UIEdgeInsets(top: 0, left: 82, bottom: 0, right: 0)
   }
 
   override func setSelected(_ selected: Bool, animated: Bool) {
--- MyLocations/LocationDetailsViewController.swift
+++ MyLocations/LocationDetailsViewController.swift
@@ -71,6 +71,18 @@ class LocationDetailsViewController: UITableViewController {
                                                    action: #selector(hideKeyboard))
     gestureRecognizer.cancelsTouchesInView = false
     tableView.addGestureRecognizer(gestureRecognizer)
+    tableView.backgroundColor = UIColor.black
+    tableView.separatorColor = UIColor(white: 1.0, alpha: 0.2)
+    tableView.indicatorStyle = .white
+    
+    descriptionTextView.textColor = UIColor.white
+    descriptionTextView.backgroundColor = UIColor.black
+    
+    addPhotoLabel.textColor = UIColor.white
+    addPhotoLabel.highlightedTextColor = addPhotoLabel.textColor
+    
+    addressLabel.textColor = UIColor(white: 1.0, alpha: 0.4)
+    addressLabel.highlightedTextColor = addressLabel.textColor
   }
   
   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -120,6 +132,32 @@ class LocationDetailsViewController: UITableViewController {
     } else if indexPath.section == 1 && indexPath.row == 0 {
       tableView.deselectRow(at: indexPath, animated: true)
       pickPhoto()
+    }
+  }
+  
+  override func tableView(_ tableView: UITableView,
+                          willDisplay cell: UITableViewCell,
+                          forRowAt indexPath: IndexPath) {
+    cell.backgroundColor = UIColor.black
+    
+    if let textLabel = cell.textLabel {
+      textLabel.textColor = UIColor.white
+      textLabel.highlightedTextColor = textLabel.textColor
+    }
+    
+    if let detailLabel = cell.detailTextLabel {
+      detailLabel.textColor = UIColor(white: 1.0, alpha: 0.4)
+      detailLabel.highlightedTextColor = detailLabel.textColor
+    }
+    
+    let selectionView = UIView(frame: CGRect.zero)
+    selectionView.backgroundColor = UIColor(white: 1.0, alpha: 0.2)
+    cell.selectedBackgroundView = selectionView
+    
+    if indexPath.row == 2 {
+      let addressLabel = cell.viewWithTag(100) as! UILabel
+      addressLabel.textColor = UIColor.white
+      addressLabel.highlightedTextColor = addressLabel.textColor
     }
   }
   
