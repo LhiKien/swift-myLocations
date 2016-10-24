8d25e02213cfafa99658b1c2cebae65d55dee334
kickinbahk <kickinbahk@gmail.com>
2016-10-24 13:47:04 -0700

    add new image for no image

--- MyLocations/LocationCell.swift
+++ MyLocations/LocationCell.swift
@@ -53,6 +53,6 @@ class LocationCell: UITableViewCell {
     if location.hasPhoto, let image = location.photoImage {
       return image.resizedImage(withBounds: CGSize(width: 52, height: 52))
     }
-    return UIImage()
+    return UIImage(named: "No Photo")!
   }
 }
